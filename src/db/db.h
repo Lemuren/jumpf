da9a83b41e00d8c5b6949c50d8c5292d2e80fd3f
Lemuren <h.e.thorsell@gmail.com>
2026-03-02 04:29:42 +0100

    Remove unnecessary sqlite import in db header

--- src/db/db.h
+++ src/db/db.h
@@ -1,8 +1,6 @@
 #ifndef DB_H
 #define DB_H
 
-#include <sqlite3.h>
-
 int db_init(const char *db_path);
 int db_update_file(const char *db_path, const char *path, int atime);
 int db_get_top(const char *db_path, int limit, char ***paths, int *count);
