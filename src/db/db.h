a6e1c52ef796468ecba8c30948a4f8700d24fe04
Lemuren <h.e.thorsell@gmail.com>
2026-03-02 04:29:11 +0100

    Remove dangling db_t typedef

--- src/db/db.h
+++ src/db/db.h
@@ -3,8 +3,6 @@
 
 #include <sqlite3.h>
 
-typedef struct db db_t;
-
 int db_init(const char *db_path);
 int db_update_file(const char *db_path, const char *path, int atime);
 int db_get_top(const char *db_path, int limit, char ***paths, int *count);
